75324da8cb7b05147ec5709b47417856f538e669
maramiia <loseva.masha24@gmail.com>
2024-09-29 17:31:21 +0500

    init

---
+++ CSharpClicerWeb/Program.cs
@@ -0,0 +1,17 @@
+namespace CSharpClicerWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddHealthChecks();
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+            app.MapHealthChecks("health-check");
+
+            app.Run();
+        }
+    }
+}
